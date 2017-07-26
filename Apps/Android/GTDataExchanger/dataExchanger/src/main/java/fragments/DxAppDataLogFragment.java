40a12516bf742dbe00f83548d6917601c1643ca1
samjjc <sam111jc@gmail.com>
2017-07-26 13:56:49 -0400

    moved andrid app directory

---
+++ Apps/Android/GTDataExchanger/dataExchanger/src/main/java/fragments/DxAppDataLogFragment.java
@@ -0,0 +1,13 @@
+package fragments;
+
+/**
+ * Created by EGUSI16 on 11/25/2016.
+ */
+
+public class DxAppDataLogFragment extends DxAppFragment {
+
+
+    private void retrieveMetasFromDataLoggerWithCompletion() {
+        dxAppController.retrieveMetasFromDataLoggerWithCompletion();
+    }
+}
